67669e19cfd72bec473bea3eeb6ed2542b95c60a
Harivansh-coder <harivanshthakur8@gmail.com>
2025-05-24 00:04:22 +0530

    fixed minor issue

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -3,7 +3,10 @@ import { Facebook, Twitter, Instagram } from "lucide-react";
 
 export default function Footer() {
   return (
-    <footer className="bg-blue-600 text-white py-8">
+    <footer
+      className="bg-blue-800 
+     text-white py-8"
+    >
       <div className="container mx-auto px-4">
         <div className="grid grid-cols-1 md:grid-cols-3 gap-8 text-center justify-items-center">
           <div>
